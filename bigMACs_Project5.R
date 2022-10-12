de1be8bf5b2a64a42b7b330859bd401435e6b4f6
chencvq <chencvq@bc.edu>
2022-10-11 20:46:40 -0400

    results + figure edits

--- bigMACs_Project5.R
+++ bigMACs_Project5.R
@@ -8,10 +8,12 @@ for(i in dat.f){
   who <- met.dat[1] #store the first element
   activity <- gsub(".csv","",met.dat[2]) #store second element, dropping CSV with gsub()
   dat.l[[i]]<- read_csv(i)%>%
-    mutate(who=who,activity=activity) #read, then store data with additional columns of metada in list as position unique to i
-  
+    mutate(who=who,activity=activity) %>% #read, then store data with additional columns of metada in list as position unique to i
+    group_by(who, activity) %>% 
+    mutate(mean = mean(degC))
 }
 dat <- do.call(rbind,dat.l) #combine the list into table
 
 dat%>%
-  ggplot()+geom_boxplot(aes(x=activity,y=degC))+facet_grid(.~who) #plot to see what we get
+  ggplot(aes(x=activity,y=degC))+geom_boxplot()+stat_summary(fun = "mean", geom = "point", shape = 23, size = 3, fill = "white")+facet_grid(.~who) #plot to see what we get
+
